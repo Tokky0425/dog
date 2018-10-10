bd4ab8c837e5a709611856b7b7c64121a7584f6e
Tokky0425 <lily.yarn.5@gmail.com>
2018-10-10 09:37:59 +0900

    first commit

---
+++ dog.go
@@ -0,0 +1,7 @@
+// Package dog implements everything from the view point of a dog.
+package dog
+
+// Years function returns an int which is multiplied by 7.
+func Years(humanYears int) int {
+	return humanYears * 7
+}
